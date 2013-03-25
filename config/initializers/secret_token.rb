ff17dc462c06a85d870af8eab6522929c4153511
Vinay <vinay59.grewal@gmail.com>
2013-03-26 00:49:43 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bulbul::Application.config.secret_token = 'd1abefe44ef00d7e6346c6c012526c35c5b2a83a0f4cd0a3ee55ac4ff3a4efca5f63f60b531f329caf4053d716112f1c3c2698f266857abbe370c8214473b1af'
